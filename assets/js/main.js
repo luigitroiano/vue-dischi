d6d2cfd57c357d0bcefd15fdbe12cd2f76fe4343
LuigiTroiano <troiano.luigi8@gmail.com>
2021-01-11 17:30:41 +0100

    Esercizio Completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,27 @@
-let app = new VTTCue({
+// Istruzioni:
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+// Servendoci di Vue JS stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+// Layout base:
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
+
+let app = new Vue({
   el: "#root",
   data: {
+    albums: [],
+    // song: "null",
+    // author: "null",
+    // year: "null",
 
   },
   mounted() {
-    var config = {
-    method: 'get',
-    url: 'https://flynn.boolean.careers/exercises/api/array/music',
-    };
-
-    axios(config)
-    .then(function (response) {
-      
+    
+    axios
+    .get("https://flynn.boolean.careers/exercises/api/array/music")
+    .then(response => {
+      this.albums = response.data.response;
     })
     .catch(function (error) {
       console.log(error);
